6e7693b8edff18d1bda65b7993529005c965c315
Dennis Ukonu <ukonudennis64@gmail.com>
2022-10-06 16:07:37 +0100

    --TokenFactory

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -2,8 +2,10 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./ERC20Token.sol";
 import "./NFTCollection.sol";
+import "./IERC20Token.sol";
 
 contract TokenFactory {
     using Counters for Counters.Counter;
@@ -20,7 +22,7 @@ contract TokenFactory {
         string name;
         string symbol;
         address owner;
-        ERC20Token contractAddr;
+        ERC20Token contractAddress;
     }
 
     struct NFTDetails {
@@ -28,20 +30,21 @@ contract TokenFactory {
         string name;
         string symbol;
         address owner;
-        NFTCollection contractAddr;
+        NFTCollection contractAddress;
     }
 
     mapping(uint256 => ERC20Details) erc20Details;
     mapping(uint256 => NFTDetails) nftDetails;
 
-    mapping(address => bool) ERC20Tokens;
-    mapping(address => bool) NFTCollections;
+    // mapping(address => bool) ERC20Tokens;
+    // mapping(address => bool) NFTCollections;
 
     function CreateContract(
         bool option,
         string memory _name,
         string memory _symbol,
-        uint256 _initialSupply
+        uint256 _initialSupply,
+        address minter
     ) public {
         bool setOption = option;
         // if true it Creates NFT contract
@@ -49,7 +52,8 @@ contract TokenFactory {
             ERC20Token erc20token = createERC20Token(
                 _name,
                 _symbol,
-                _initialSupply
+                _initialSupply,
+                minter
             );
             _tokenCount.increment();
             uint256 id = _tokenCount.current();
@@ -78,106 +82,88 @@ contract TokenFactory {
         }
     }
 
+    function createERC20Token(
+        string memory _name,
+        string memory _symbol,
+        uint256 _initialSupply,
+        address minter
+    ) internal returns (ERC20Token) {
+        return new ERC20Token(_initialSupply, _name, _symbol, minter);
+    }
+
     function createNFTCollection(string memory _name, string memory _symbol)
-        private
+        internal
         returns (NFTCollection)
     {
         return new NFTCollection(_name, _symbol);
     }
 
-    function createERC20Token(
-        string memory _name,
-        string memory _symbol,
-        uint256 _initialSupply
-    ) private returns (ERC20Token) {
-        return new ERC20Token(_initialSupply, _name, _symbol);
-    }
-
     // CONTRACT METHODS
-    function ownerOfERC20Contract(uint256 index)
+
+    function getERC20Details(uint256 index)
         external
         view
-        returns (address)
+        returns (
+            ERC20Token,
+            string memory,
+            string memory,
+            address
+        )
     {
         require(
             index == erc20Details[index].id,
-            "Owner with this address does not exist"
+            "Owner with this id does not exist"
         );
-        return erc20Details[index].owner;
+        string memory _name = erc20Details[index].name;
+        string memory _symbol = erc20Details[index].symbol;
+        address _owner = erc20Details[index].owner;
+        ERC20Token _contractAddress = erc20Details[index].contractAddress;
+        return (_contractAddress, _name, _symbol, _owner);
     }
 
-    function ownerOfNFTContract(uint256 index) external view returns (address) {
-        require(
-            index == nftDetails[index].id,
-            "Owner with this address does not exist"
-        );
-        return nftDetails[index].owner;
-    }
-
-    function erc20ContractAddressByIndex(uint256 index)
+    function getNFTDetails(uint256 index)
         external
         view
-        returns (ERC20Token)
-    {
-        require(
-            index == erc20Details[index].id,
-            "Owner with this address does not exist"
-        );
-        return erc20Details[index].contractAddr;
-    }
-
-    function nftContractAddressByIndex(uint256 index)
-        external
-        view
-        returns (NFTCollection)
+        returns (
+            NFTCollection,
+            string memory,
+            string memory,
+            address
+        )
     {
         require(
             index == nftDetails[index].id,
-            "Owner with this address does not exist"
+            "Owner with this id does not exist"
         );
-        return nftDetails[index].contractAddr;
+        string memory _name = nftDetails[index].name;
+        string memory _symbol = nftDetails[index].symbol;
+        address _owner = nftDetails[index].owner;
+        NFTCollection _contractAddress = nftDetails[index].contractAddress;
+        return (_contractAddress, _name, _symbol, _owner);
     }
 
-    // function getERC20Details(uint256 index)
-    //     external
-    //     view
-    //     returns (
-    //         ERC20Token,
-    //         string memory,
-    //         string memory,
-    //         address
-    //     )
-    // {
-    //     require(
-    //         index == erc20Details[index].id,
-    //         "Owner with this id does not exist"
-    //     );
-    //     string memory _name = erc20Details[index].name;
-    //     string memory _symbol = erc20Details[index].symbol;
-    //     address _owner = erc20Details[index].owner;
-    //     ERC20Token _contractAddress = erc20Details[index].contractAddr;
-    //     return (_contractAddress, _name, _symbol, _owner);
+    // function name (uint256 index)public view returns(string memory){
+    //     return erc20Details[index].name;
     // }
 
-    //   function getNFTDetails(uint256 index)
-    //     external
-    //     view
-    //     returns (
-    //         NFTCollection,
-    //         string memory,
-    //         string memory,
-    //         address
-    //     )
-    // {
+    // function symbol (uint256 index)public view returns(string memory){
+    //     return erc20Details[index].symbol;
+    // }
+
+    // function erc20ContractAddressByIndex(uint256 index) external view returns (ERC20Token) {
+    //     require(
+    //         index == erc20Details[index].id,
+    //         "Owner with this address does not exist"
+    //     );
+    //     return erc20Details[index].contractAddress;
+    // }
+
+    //  function nftContractAddressByIndex(uint256 index) external view returns (NFTCollection) {
     //     require(
     //         index == nftDetails[index].id,
-    //         "Owner with this id does not exist"
+    //         "Owner with this address does not exist"
     //     );
-    //     string memory _name = nftDetails[index].name;
-    //     string memory _symbol = nftDetails[index].symbol;
-    //     address _owner = nftDetails[index].owner;
-    //     NFTCollection _contractAddress = nftDetails[index].contractAddr;
-    //     return (_contractAddress, _name, _symbol, _owner);
+    //     return nftDetails[index].contractAddress;
     // }
 
     // function isERC20ContractOwner(address _owner, uint256 index)
@@ -202,5 +188,63 @@ contract TokenFactory {
     //         "Owner with this address and id does not exist"
     //     );
     //     return (true);
+    // }
+
+    // function ownerOfERC20Contract(uint256 index) external view returns (address) {
+    //     require(
+    //         index == erc20Details[index].id,
+    //         "Owner with this address does not exist"
+    //     );
+    //     return erc20Details[index].owner;
+    // }
+
+    //  function ownerOfNFTContract(uint256 index) external view returns (address) {
+    //     require(
+    //         index == nftDetails[index].id,
+    //         "Owner with this address does not exist"
+    //     );
+    //     return nftDetails[index].owner;
+    // }
+
+    // ERC20 INTERFACE
+    // function mintERC20( address contractAddress, address to, uint256 amount) public{
+    //     //  ERC20Token contractAddress = erc20Details[index].contractAddress;
+    //     IERC20Token(contractAddress).mintToken(to, amount);
+    // }
+
+    // function totalSupply(uint256 index)public view returns(uint256){
+    // ERC20Token contractAddress = erc20Details[index].contractAddress;
+    // // address contractAddress = wallets[index];
+    // return IERC20(contractAddress).totalSupply();
+    // }
+
+    // function balanceOf(uint256 index, address owner)public view returns(uint256){
+    //  ERC20Token contractAddress = erc20Details[index].contractAddress;
+    // // address contractAddress = wallets[index];
+    // return IERC20(contractAddress).balanceOf(owner);
+    // }
+
+    // function transfer(uint256 index, address receiver, uint256 amount)public returns(bool){
+    // ERC20Token contractAddress = erc20Details[index].contractAddress;
+    // // address contractAddress = wallets[index];
+    // return IERC20(contractAddress).transfer(receiver,amount);
+    // }
+
+    // function transferFrom(uint256 index, address sender, address receiver, uint256 amount)public returns(bool){
+    // ERC20Token contractAddress = erc20Details[index].contractAddress;
+    // // address contractAddress = wallets[index];
+    // return IERC20(contractAddress).transferFrom(sender,receiver, amount);
+    // }
+
+    // function allowance(uint256 index, address owner, address spender)public view returns(uint256){
+    // ERC20Token contractAddress = erc20Details[index].contractAddress;
+    // // address contractAddress = wallets[index];
+    // return IERC20(contractAddress). allowance(owner, spender);
+    // }
+
+    //   function approve(uint256 index, address spender, uint256 amount)public returns(bool){
+    // ERC20Token contractAddress = erc20Details[index].contractAddress;
+    // // address contractAddress = wallets[index];
+    // return IERC20(contractAddress). approve(spender, amount);
     // }
 }
